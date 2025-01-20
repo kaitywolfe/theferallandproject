d1236669584438323386948dbabbd960a1be4d82
Andrew Cornell <andrewcornell@Andrews-MacBook-Pro.local>
2025-01-20 11:08:44 -0800

    remove basename router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-
 function App() {
   return (
-    <Router basename="/theferallandproject">
-      <div className="bg-black min-h-lvh">
-        <div className="flex justify-center items-center bg-[url('./assets/background.jpg')] min-h-svh bg-no-repeat bg-cover">
-          <div className=''>
-            <div className='flex flex-col px-16 py-10 bg-slate-950 rounded-lg w-[400px] m-auto text-white font-mutlu text-6xl'>
-              <h1 className='pl-3 text-5xl'>The</h1>
-              <h1>Feral Land</h1>
-              <h1 className='pl-4 pt-4'>Project</h1>
-              <h1 className="pt-4 text-4xl font-semibold font-sans">Coming Soon!</h1>
-            </div>
+    <div className="bg-black min-h-lvh">
+      <div className="flex justify-center items-center bg-[url('./assets/background.jpg')] min-h-svh bg-no-repeat bg-cover">
+        <div className=''>
+          <div className='flex flex-col px-16 py-10 bg-slate-950 rounded-lg w-[400px] m-auto text-white font-mutlu text-6xl'>
+            <h1 className='pl-3 text-5xl'>The</h1>
+            <h1>Feral Land</h1>
+            <h1 className='pl-4 pt-4'>Project</h1>
+            <h1 className="pt-4 text-4xl font-semibold font-sans">Coming Soon!</h1>
           </div>
         </div>
       </div>
-    </Router >
+    </div>
   )
 }
 
