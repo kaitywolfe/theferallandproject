b42ccd45b4c180ae84fbaed0fc1159bf2eea7582
Andrew Cornell <andrewcornell@Andrews-MacBook-Pro.local>
2025-01-19 18:05:31 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: mode === 'production' ? '/theferallandproject/' : '/'
-    // base: mode === 'production' ? '/theferallandproject/' : '/'
+    base: '/',
   };
 })
